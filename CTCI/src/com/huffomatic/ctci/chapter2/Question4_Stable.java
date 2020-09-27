c52365fb8f4184c1b055c82e811f9e20d4b72027
Michael Plain <Michael Plain@huffomatic-pc>
2020-09-26 22:05:09 -0400

    Added a stable solution.

--- CTCI/src/com/huffomatic/ctci/chapter2/Question4_Stable.java
+++ CTCI/src/com/huffomatic/ctci/chapter2/Question4_Stable.java
@@ -9,11 +9,11 @@ import com.huffomatic.ctci.common.util.LinkedListUtil;
 /**
  * Partition a linked list around a value x.
  * The left partition is less than x.
- * The right partition is greater than or equal to x.
- * The value x can appear anywhere in the right partition.
+ * The middle partition is equal to x.
+ * The right partition is greater than x.
  * 
- * Input:  (3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1)
- * Output: (3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8)
+ * Input:  (3 -> 5 -> 8 -> 5 -> 10 -> 2 -> 1), x = 5
+ * Output: (3 -> 1 -> 2 -> 5 -> 5 -> 10 -> 8)
  * 
  * Time:  O(length of list)
  * Space: O(1)
@@ -21,22 +21,25 @@ import com.huffomatic.ctci.common.util.LinkedListUtil;
  * @author huffomatic
  *
  */
-public class Question4_MostlyStable {
+public class Question4_Stable {
 	public static void main(String[] argv) {
 		Node result1 = partition(Scenario.scenario5_list1(), 5);
-		Assert.assertTrue(LinkedListUtil.compareLists(result1, Scenario.result5()));
+		Assert.assertTrue(LinkedListUtil.compareLists(result1, Scenario.result8()));
 		
 		Node result2 = partition(Scenario.scenario5_list1(), 3);
-		Assert.assertTrue(LinkedListUtil.compareLists(result2, Scenario.result6()));
+		Assert.assertTrue(LinkedListUtil.compareLists(result2, Scenario.result9()));
 		
 		Node result3 = partition(Scenario.scenario5_list1(), 8);
-		Assert.assertTrue(LinkedListUtil.compareLists(result3, Scenario.result7()));
+		Assert.assertTrue(LinkedListUtil.compareLists(result3, Scenario.result10()));
 		
 		Node result4 = partition(Scenario.scenario5_list1(), 1);
-		Assert.assertTrue(LinkedListUtil.compareLists(result4, Scenario.scenario5_list1()));
+		Assert.assertTrue(LinkedListUtil.compareLists(result4, Scenario.result11()));
 		
 		Node result5 = partition(Scenario.scenario5_list1(), 11);
 		Assert.assertTrue(LinkedListUtil.compareLists(result5, Scenario.scenario5_list1()));
+		
+		Node result6 = partition(Scenario.scenario5_list1(), 7);
+		Assert.assertTrue(LinkedListUtil.compareLists(result6, Scenario.result10()));
 	}
 	
 	public static Node partition(Node head, int x) {
@@ -47,6 +50,8 @@ public class Question4_MostlyStable {
 		Node runner = head;
 		Node smallerStart = null;
 		Node smallerEnd = null;
+		Node middleStart = null;
+		Node middleEnd = null;
 		Node largerStart = null;
 		Node largerEnd = null;
 		
@@ -63,6 +68,16 @@ public class Question4_MostlyStable {
 					smallerEnd = runner;
 				}
 			}
+			else if (runner.data == x) {
+				if (middleStart == null) {
+					middleStart = runner;
+					middleEnd = runner;
+				}
+				else {
+					middleEnd.next = runner;
+					middleEnd = runner;
+				}
+			}
 			else {
 				if (largerStart == null) {
 					largerStart = runner;
@@ -77,11 +92,24 @@ public class Question4_MostlyStable {
 		}
 		
 		if (smallerStart == null) {
-			return largerStart;
+			if (middleStart == null) {
+				return largerStart;
+			}
+			else {
+				middleEnd.next = largerStart;
+				return middleStart;
+			}
 		}
 		else {
-			smallerEnd.next = largerStart;
-			return smallerStart;
+			if (middleStart == null) {
+				smallerEnd.next = largerStart;
+				return smallerStart;
+			}
+			else {
+				smallerEnd.next = middleStart;
+				middleEnd.next = largerStart;
+				return smallerStart;
+			}
 		}
 	}
 }
--- CTCI/src/com/huffomatic/ctci/common/scenario/Scenario.java
+++ CTCI/src/com/huffomatic/ctci/common/scenario/Scenario.java
@@ -254,4 +254,104 @@ public class Scenario {
 		
 		return n1;
 	}
+	
+	public static Node result8() {
+		Node n1 = new Node(3);
+		Node n2 = new Node(2);
+		Node n3 = new Node(1);
+		Node n4 = new Node(5);
+		Node n5 = new Node(5);
+		Node n6 = new Node(8);
+		Node n7 = new Node(10);
+		
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		n5.next = n6;
+		n6.next = n7;
+		n7.next = null;
+		
+		return n1;
+	}
+	
+	public static Node result9() {
+		Node n1 = new Node(2);
+		Node n2 = new Node(1);
+		Node n3 = new Node(3);
+		Node n4 = new Node(5);
+		Node n5 = new Node(8);
+		Node n6 = new Node(5);
+		Node n7 = new Node(10);
+		
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		n5.next = n6;
+		n6.next = n7;
+		n7.next = null;
+		
+		return n1;
+	}
+	
+	public static Node result10() {
+		Node n1 = new Node(3);
+		Node n2 = new Node(5);
+		Node n3 = new Node(5);
+		Node n4 = new Node(2);
+		Node n5 = new Node(1);
+		Node n6 = new Node(8);
+		Node n7 = new Node(10);
+		
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		n5.next = n6;
+		n6.next = n7;
+		n7.next = null;
+		
+		return n1;
+	}
+	
+	public static Node result11() {
+		Node n1 = new Node(1);
+		Node n2 = new Node(3);
+		Node n3 = new Node(5);
+		Node n4 = new Node(8);
+		Node n5 = new Node(5);
+		Node n6 = new Node(10);
+		Node n7 = new Node(2);
+		
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		n5.next = n6;
+		n6.next = n7;
+		n7.next = null;
+		
+		return n1;
+	}
+	
+	public static Node result12() {
+		Node n1 = new Node(3);
+		Node n2 = new Node(5);
+		Node n4 = new Node(5);
+		Node n6 = new Node(2);
+		Node n7 = new Node(1);
+		Node n3 = new Node(8);
+		Node n5 = new Node(10);
+		
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = n5;
+		n5.next = n6;
+		n6.next = n7;
+		n7.next = null;
+		
+		return n1;
+	}
 }
